8d30b2a845f49eae8d6baeccff01aadd5344d19c
doyeonkim-cubox-ai <doyeon.kim@cubox.ai>
2024-11-18 10:00:30 +0900

    FEAT : add save dir

--- FaceDetection/inference.py
+++ FaceDetection/inference.py
@@ -4,7 +4,7 @@ from ultralytics import YOLO
 
 def main():
     model = YOLO("./FaceDetection/yolo11/weights/best.pt")
-    model.predict("https://ultralytics.com/images/bus.jpg", save=True)
+    model.predict("https://ultralytics.com/images/bus.jpg", save=True, project="./result", name="yolo11")
 
 
 if __name__ == '__main__':
